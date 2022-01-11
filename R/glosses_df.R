7f3bc824222ba8ba25789265e46cf6e66666d3e6
sverhees <jh.verhees@gmail.com>
2022-01-11 21:37:36 +0300

    corrected

--- R/glosses_df.R
+++ R/glosses_df.R
@@ -1,15 +1,15 @@
 #' Catalog of glosses
 #'
-#' A dataset contains the list of glosses from the Leipzig Glossing Rules by
-#' Comrie, Haspelmath, and Bickel and other glosses automatically gathered from
-#' Glossa Journal articles.
+#' A dataset containing the list of glosses from the Leipzig Glossing Rules by
+#' Comrie, Haspelmath, and Bickel, as well as other glosses gathered automatically from
+#' articles in the Open Access journal Glossa.
 #'
 #' @format A data frame with 1339 rows and 4 variables:
 #' \describe{
 #'   \item{gloss}{the gloss abbreviation}
 #'   \item{definition}{the gloss definition}
 #'   \item{source}{the gloss source. Three possible values: Leipzig Glossing Rules, \href{https://en.wikipedia.org/wiki/List_of_glossing_abbreviations}{Wikipedia} or lingglosses (this means parsed from Glossa).}
-#'   \item{weight}{glossa weight used for the choice in case of multiple definitions per gloss.}
+#'   \item{weight}{gloss weight used for the choice in case of multiple definitions per gloss.}
 #' }
 #'
 
